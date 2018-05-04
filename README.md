a0aeb493cfb32e97fd6d2119d5dd821e7d231842
Abhijit Rahate <ARahate@chenoainc.com>
2018-05-04 13:17:16 +0530

    create packages with bat file

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # testframework
+1.Build solution in "Release" configuration.
+2.Run pack.bat to create nuget packages.
+
---
+++ pack.bat
@@ -0,0 +1,2 @@
+nuget pack CafeNextFramework\CafeNextFramework.nuspec -OutputDirectory lib
+nuget pack CafeNextFramework.Helpers\CafeNextFramework.Helpers.nuspec -OutputDirectory lib
